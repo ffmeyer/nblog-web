3e010d1486f61069ee7a70415cf97180e5fc7c80
Felipe Meyer <ffmeyer@gmail.com>
2017-08-26 15:21:29 -0300

    fork papito project

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -1,3 +1,4 @@
+require 'cucumber'
 require 'capybara'
 require 'capybara/cucumber'
 require 'capybara/poltergeist'
@@ -11,18 +12,19 @@ $env = ENV['ENVIRONMENT']
 $data = YAML.load_file(File.dirname(__FILE__) + '/env.yaml')
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Poltergeist::Driver.new(
-    app,
-    options = {
-      js_errors: false
-    }
-  )
+    Capybara::Poltergeist::Driver.new(
+        app, 
+        options = {
+            :js_errors => false
+        }
+    )
 end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
-  config.app_host = $data['url'][$env]
-end
 
+    config.default_driver = :selenium 
+    config.app_host = $data['url'][$env]
+    
+end
 
 Capybara.default_max_wait_time = 5
